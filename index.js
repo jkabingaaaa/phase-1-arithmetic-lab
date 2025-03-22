2fd66139cd67d8e1524635caba33ed8ab15ecb0f
Joseph <joseph.kabinga@student.moringaschool.com>
2025-03-22 12:16:13 +0300

    Adds phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 =31;
+const num2=2;
+var multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+
+const num3 =20;
+const num4 =8;
+var mod = num3 % num4;
+
+var max = Math.max(16,17,20);
